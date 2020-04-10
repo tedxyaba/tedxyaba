4fa7d40863d79a154b3ff9e71d1c2cbdaba3faff
Sunday Adefila <adefilaedward@gmail.com>
2020-04-11 00:57:39 +0100

    access control headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_access_control_headers
+
+  private
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+  end
 end
