a16b94277c70a1ac8ec305b6f08d789560227463
Sunday Adefila <adefilaedward@gmail.com>
2020-05-24 12:05:50 +0100

    parsing params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_access_control_headers
+  before_action :format_filters
 
   private
   def set_access_control_headers
@@ -8,5 +9,12 @@ class ApplicationController < ActionController::Base
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+  end
+
+  def format_filters
+    params[:filters] ||= {}
+    if params[:filters].is_a?(String)
+      params[:filters] = JSON.parse(params[:filters])
+    end
   end
 end
