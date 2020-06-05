e98d10798ca1dfd06cac2b1399cb2c66efe92ea8
Sunday Adefila <adefilaedward@gmail.com>
2020-06-05 04:59:37 +0100

    temp dissable posting subs to mailchimp

--- app/models/newsletter_subscription.rb
+++ app/models/newsletter_subscription.rb
@@ -1,30 +1,30 @@
 class NewsletterSubscription < ApplicationRecord
-  include HTTParty
-  base_uri "https://us15.api.mailchimp.com/3.0"
+  # include HTTParty
+  # base_uri "https://us15.api.mailchimp.com/3.0"
 
   validates_uniqueness_of :email
 
-  before_save :post_sub_to_mailchimp
+  # before_save :post_sub_to_mailchimp
 
-  private
-  def post_sub_to_mailchimp
-    mailchimp_api_key = Rails.application.credentials.dig(:mailchimp, :api_key)
-    list_id = Rails.application.credentials.dig(:mailchimp, :list_id)
-    auth = { username: 'tedxyaba-web', password: mailchimp_api_key }
-    body = {
-      email_address: email,
-      status: 'unsubscribed',
-      tags: %w(TEDxYabaWeb-v2 NewsLetterSub)
-    }
+  # private
+  # def post_sub_to_mailchimp
+  #   mailchimp_api_key = Rails.application.credentials.dig(:mailchimp, :api_key)
+  #   list_id = Rails.application.credentials.dig(:mailchimp, :list_id)
+  #   auth = { username: 'tedxyaba-web', password: mailchimp_api_key }
+  #   body = {
+  #     email_address: email,
+  #     status: 'unsubscribed',
+  #     tags: %w(TEDxYabaWeb-v2 NewsLetterSub)
+  #   }
 
-    res = self.class.post("/lists/#{list_id}/members", {
-      basic_auth: auth,
-      body: body.to_json,
-      headers: { 'Content-Type' => 'application/json' }
-    })
+  #   res = self.class.post("/lists/#{list_id}/members", {
+  #     basic_auth: auth,
+  #     body: body.to_json,
+  #     headers: { 'Content-Type' => 'application/json' }
+  #   })
 
-    self.email = "failed_#{self.email}" unless res.code == 200
-  rescue HTTParty::Error
-    self.email = "failed_#{self.email}"
-  end
+  #   self.email = "failed_#{self.email}" unless res.code == 200
+  # rescue HTTParty::Error
+  #   self.email = "failed_#{self.email}"
+  # end
 end
