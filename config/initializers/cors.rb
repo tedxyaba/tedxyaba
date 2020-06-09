ca2af61f1ce8356f6622dbda80c0ecbd68523e33
Sunday Adefila <adefilaedward@gmail.com>
2020-06-09 11:48:24 +0100

    rearrange

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,9 +5,9 @@
 Rails.application.config.action_dispatch.cookies_serializer = :json
 
 # Rails 6 has support for blocking requests from unknown hosts, so origin domains will need to be added there as well.
-Rails.application.config.hosts << "tedxyaba.com"
 Rails.application.config.hosts << "be.tedxyaba.com"
-Rails.application.config.hosts << "tedxyaba-web.herokuapp.com"
 Rails.application.config.hosts << "tedxyaba.herokuapp.com"
-# will see how localhost works
-# Rails.application.config.hosts << "tedxyaba.com"
+
+# may not need these
+Rails.application.config.hosts << "tedxyaba.com"
+Rails.application.config.hosts << "tedxyaba-web.herokuapp.com"
